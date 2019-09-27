5d92029d79446da459641136987d1e8036f990cf
Ny Hasinavalona Randriantsarafara <randriantsarafaranyhasinavalon@gmail.com>
2019-09-27 13:52:42 +0300

    [APP] Setup component minimal logic #master

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,76 @@
 import { Component } from '@angular/core';
+import { NestedTreeControl } from '@angular/cdk/tree';
+import { ArrayDataSource } from '@angular/cdk/collections';
+
+interface Node<T> {
+    children?: T[];
+}
+
+interface Category extends Node<Category> {
+    name: string;
+    parent?: string;
+}
+
+const TREE_DATA: Category[] = [
+    {
+        name: 'Pizza',
+        children: [
+            {
+                name: 'Pizza GM',
+                parent: 'Pizza',
+                children: [
+                    {
+                        name: 'Pizza sans porc',
+                        parent: 'Pizza GM'
+                    },
+                    {
+                        name: 'Pizza avec porc',
+                        parent: 'Pizza GM'
+                    }
+                ]
+            },
+            {
+                name: 'Pizza PM',
+                parent: 'Pizza',
+                children: [
+                    {
+                        name: 'Pizza sans porc',
+                        parent: 'Pizza PM'
+                    },
+                    {
+                        name: 'Pizza avec porc',
+                        parent: 'Pizza PM'
+                    }
+                ]
+            }
+        ]
+    },
+    {
+        name: 'Glace'
+    },
+    {
+        name: 'Boissons',
+        children: [
+            {
+                name: 'Boissons PM',
+                parent: 'Boisson'
+            },
+            {
+                name: 'Boissons GM',
+                parent: 'Boisson'
+            }
+        ]
+    }
+];
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'angular-tree-grid';
+    treeControl = new NestedTreeControl<Category>(node => node.children);
+    dataSource = new ArrayDataSource(TREE_DATA);
+    hasChildren = (_: number, node: Category) =>
+        !!node.children && node.children.length;
 }
