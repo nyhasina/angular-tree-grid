4567ba8a469de6578880bdefaeca5ba45a7d6c19
Ny Hasinavalona Randriantsarafara <randriantsarafaranyhasinavalon@gmail.com>
2019-09-27 16:10:51 +0300

    [APP] Register tree grid component #master

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,12 @@ import {NgModule} from '@angular/core';
 import {AppRoutingModule} from './app-routing.module';
 import {AppComponent} from './app.component';
 import {CdkTreeModule} from '@angular/cdk/tree';
+import { TreeGridComponent } from './tree-grid/tree-grid.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TreeGridComponent
   ],
   imports: [
     BrowserModule,
